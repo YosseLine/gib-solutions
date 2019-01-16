3445b6bd555d8d0d3c058a2872913a95e8af47be
Yosseline <yoseline_96@hotmail.com>
2019-01-15 21:45:34 -0500

    Añdiendo efecto scroll en header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
   $('.dropdown-toggle').dropdown();
   $('.carousel').carousel();
+
+  var flag = false;
+  var scroll;
+
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+
+    if(scroll > 200){
+      if(!flag){
+        $("#navbar").css({"background-color": "rgb(231,231,231)"});
+        flag = true;
+      }
+    } else{
+      if(flag){
+        $("#navbar").css({"background-color": "rgba(231,231,231,0.5)"});
+        flag = false;
+      }
+    }
+  });
 });
